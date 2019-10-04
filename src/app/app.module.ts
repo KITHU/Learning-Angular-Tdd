ee9d1320751aa464acac9f6993f23db22afc0f69
KITHU <james.kithu2@gmail.com>
2019-10-04 11:23:19 +0300

    - added star-component as a nested component to product-list-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { starComponent } from './shared/star.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    starComponent
   ],
   imports: [
     BrowserModule,
